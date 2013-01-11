0e614ac98e1e2222e5da0028edbfa5130e0b4c3d
Ivan Kuchin <tadump+git@gmail.com>
2013-01-11 02:12:05 +0100

    spec cleanup/regorganization

--- spec/image_optim_spec.rb
+++ spec/image_optim_spec.rb
@@ -1,39 +1,9 @@
 $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require 'rspec'
 require 'image_optim'
+require 'tempfile'
 
-spec_dir = ImageOptim::ImagePath.new(__FILE__).dirname.relative_path_from(Dir.pwd)
-image_dir = spec_dir / 'images'
-
-def temp_copy_path(original)
-  original.class.temp_dir do |dir|
-    temp_path = dir / original.basename
-    original.copy(temp_path)
-    yield temp_path
-  end
-end
-
-class Tempfile
-  alias_method :initialize_orig, :initialize
-
-  def initialize(*args, &block)
-    self.class.initialize_called
-    initialize_orig(*args, &block)
-  end
-
-  def self.initialize_called
-    @@call_count ||= 0
-    @@call_count += 1
-  end
-
-  def self.reset_call_count
-    @@call_count = 0
-  end
-
-  def self.call_count
-    @@call_count
-  end
-end
+TEST_IMAGES = (ImageOptim::ImagePath.new(__FILE__).dirname.relative_path_from(Dir.pwd) / 'images').glob('*')
 
 Fixnum.class_eval do
   def in_range?(range)
@@ -41,76 +11,110 @@ Fixnum.class_eval do
   end
 end
 
+Tempfile.class_eval do
+  def self.init_count
+    @@init_count
+  end
+
+  def self.reset_init_count
+    @@init_count = 0
+  end
+
+  reset_init_count
+
+  alias_method :initialize_orig, :initialize
+  def initialize(*args, &block)
+    @@init_count += 1
+    initialize_orig(*args, &block)
+  end
+end
+
+ImageOptim::ImagePath.class_eval do
+  def temp_copy
+    temp_path.tap{ |path| copy(path) }
+  end
+end
+
 describe ImageOptim do
-  image_dir.glob('*') do |original|
-    describe "optimizing #{original}" do
-      it "should optimize image" do
-        temp_copy_path(original) do |unoptimized|
-          Tempfile.reset_call_count
+  describe "isolated" do
+    describe "optimize copy" do
+      TEST_IMAGES.each do |original|
+        it "should optimize #{original}" do
+          copy = original.temp_copy
+
+          Tempfile.reset_init_count
           image_optim = ImageOptim.new
-          optimized = image_optim.optimize_image(unoptimized)
-          optimized.should be_a(FSPath)
-          unoptimized.read.should == original.read
-          optimized.size.should > 0
-          optimized.size.should < unoptimized.size
-          optimized.read.should_not == unoptimized.read
-          if image_optim.workers_for_image(unoptimized).length > 1
-            Tempfile.call_count.should be_in_range(1..2)
+          optimized = image_optim.optimize_image(copy)
+          optimized.should be_a(ImageOptim::ImagePath)
+          optimized.size.should be_in_range(1...original.size)
+          optimized.read.should_not == original.read
+          copy.read.should == original.read
+
+          if image_optim.workers_for_image(original).length > 1
+            Tempfile.init_count.should be_in_range(1..2)
           else
-            Tempfile.call_count.should === 1
+            Tempfile.init_count.should === 1
           end
         end
       end
+    end
 
-      it "should optimize image in place" do
-        temp_copy_path(original) do |path|
-          Tempfile.reset_call_count
+    describe "optimize in place" do
+      TEST_IMAGES.each do |original|
+        it "should optimize #{original}" do
+          copy = original.temp_copy
+
+          Tempfile.reset_init_count
           image_optim = ImageOptim.new
-          image_optim.optimize_image!(path).should be_true
-          path.size.should > 0
-          path.size.should < original.size
-          path.read.should_not == original.read
-          if image_optim.workers_for_image(path).length > 1
-            Tempfile.call_count.should be_in_range(2..3)
+          image_optim.optimize_image!(copy).should be_true
+          copy.size.should be_in_range(1...original.size)
+          copy.read.should_not == original.read
+
+          if image_optim.workers_for_image(original).length > 1
+            Tempfile.init_count.should be_in_range(2..3)
           else
-            Tempfile.call_count.should === 2
+            Tempfile.init_count.should === 2
           end
         end
       end
+    end
 
-      it "should stop optimizing" do
-        temp_copy_path(original) do |unoptimized|
-          count = (1..10).find do |i|
-            unoptimized = ImageOptim.optimize_image(unoptimized)
-            unoptimized.nil?
+    describe "stop optimizing" do
+      TEST_IMAGES.each do |original|
+        it "should stop optimizing #{original}" do
+          copy = original.temp_copy
+
+          tries = 0
+          10.times do
+            tries += 1
+            break unless ImageOptim.optimize_image!(copy)
           end
-          count.should >= 2
-          count.should < 10
+          tries.should be_in_range(2...3)
         end
       end
     end
   end
 
-  describe "unsupported file" do
+  describe "unsupported" do
     let(:original){ ImageOptim::ImagePath.new(__FILE__) }
 
     it "should ignore" do
-      temp_copy_path(original) do |unoptimized|
-        Tempfile.reset_call_count
-        optimized = ImageOptim.optimize_image(unoptimized)
-        Tempfile.call_count.should == 0
-        optimized.should be_nil
-        unoptimized.read.should == original.read
-      end
+      copy = original.temp_copy
+
+      Tempfile.reset_init_count
+      optimized = ImageOptim.optimize_image(copy)
+      Tempfile.init_count.should == 0
+      optimized.should be_nil
+      copy.read.should == original.read
     end
 
     it "should ignore in place" do
-      temp_copy_path(original) do |unoptimized|
-        Tempfile.reset_call_count
-        ImageOptim.optimize_image!(unoptimized).should_not be_true
-        Tempfile.call_count.should == 0
-        unoptimized.read.should == original.read
-      end
+      copy = original.temp_copy
+
+      Tempfile.reset_init_count
+      ImageOptim.optimize_image!(copy).should_not be_true
+      Tempfile.init_count.should == 0
+      copy.read.should == original.read
     end
   end
 
@@ -124,32 +128,32 @@ describe ImageOptim do
     end
 
     %w[optimize_images optimize_images!].each do |list_method|
-      single_method = list_method.sub('images', 'image')
-      describe "without block" do
-        it "should optimize images and return array of results" do
-          image_optim = ImageOptim.new
-          dsts = []
-          srcs.each do |src|
-            dst = "#{src}_"
-            image_optim.should_receive(single_method).with(src).and_return(dst)
-            dsts << dst
+      describe list_method do
+        single_method = list_method.sub('images', 'image')
+        describe "without block" do
+          it "should optimize images and return array of results" do
+            image_optim = ImageOptim.new
+            dsts = srcs.map do |src|
+              dst = "#{src}_"
+              image_optim.should_receive(single_method).with(src).and_return(dst)
+              dst
+            end
+            image_optim.send(list_method, srcs).should == dsts
           end
-          image_optim.send(list_method, srcs).should == dsts
         end
-      end
 
-      describe "given block" do
-        it "should optimize images, yield path and result for each and return array of yield results" do
-          image_optim = ImageOptim.new
-          results = []
-          srcs.each do |src|
-            dst = "#{src}_"
-            image_optim.should_receive(single_method).with(src).and_return(dst)
-            results << "#{src} #{dst}"
+        describe "given block" do
+          it "should optimize images, yield path and result for each and return array of yield results" do
+            image_optim = ImageOptim.new
+            results = srcs.map do |src|
+              dst = "#{src}_"
+              image_optim.should_receive(single_method).with(src).and_return(dst)
+              "#{src} #{dst}"
+            end
+            image_optim.send(list_method, srcs) do |src, dst|
+              "#{src} #{dst}"
+            end.should == results
           end
-          image_optim.send(list_method, srcs) do |src, dst|
-            "#{src} #{dst}"
-          end.should == results
         end
       end
     end
