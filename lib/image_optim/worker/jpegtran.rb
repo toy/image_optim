f074c437aed6ffa00113d166d93deae536c188fb
Ivan Kuchin <tadump+git@gmail.com>
2013-03-19 01:33:23 +0100

    simple jpegrescan support

--- lib/image_optim/worker/jpegtran.rb
+++ lib/image_optim/worker/jpegtran.rb
@@ -9,11 +9,21 @@ class ImageOptim
       # Create progressive JPEG file (defaults to true)
       attr_reader :progressive
 
+      # Use jpegtran through jpegrescan, ignore progressive option (defaults to true)
+      attr_reader :jpegrescan
+
       def optimize(src, dst)
-        args = %W[-optimize -outfile #{dst} #{src}]
-        args.unshift '-copy', copy_chunks ? 'all' : 'none'
-        args.unshift '-progressive' if progressive
-        execute(:jpegtran, *args) && optimized?(src, dst)
+        if jpegrescan
+          args = %W[#{dst} #{src}]
+          args.unshift '-s' unless copy_chunks
+          resolve_bin!(:jpegtran)
+          execute(:jpegrescan, *args) && optimized?(src, dst)
+        else
+          args = %W[-optimize -outfile #{dst} #{src}]
+          args.unshift '-copy', copy_chunks ? 'all' : 'none'
+          args.unshift '-progressive' if progressive
+          execute(:jpegtran, *args) && optimized?(src, dst)
+        end
       end
 
     private
@@ -21,6 +31,7 @@ class ImageOptim
       def parse_options(options)
         get_option!(options, :copy_chunks, false){ |v| !!v }
         get_option!(options, :progressive, true){ |v| !!v }
+        get_option!(options, :jpegrescan, true){ |v| !!v }
       end
     end
   end
