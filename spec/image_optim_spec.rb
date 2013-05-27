0ce24ddb1c35809b06f586e1480c3d0d64e4bf52
Ivan Kuchin <tadump+git@gmail.com>
2013-05-28 01:17:14 +0200

    auto orienting spec

--- spec/image_optim_spec.rb
+++ spec/image_optim_spec.rb
@@ -275,4 +275,17 @@ describe ImageOptim do
       end
     end
   end
+
+  describe "auto orienting" do
+    original = ImageOptim::ImagePath.new(__FILE__).dirname / 'images/orient/original.png'
+    ImageOptim::ImagePath.new(__FILE__).dirname.glob('images/orient/*.jpg').each do |jpg|
+      it "should rotate #{jpg}" do
+        image_optim = ImageOptim.new
+        oriented = image_optim.optimize_image(jpg)
+        nrmse = `compare -metric RMSE #{original.to_s.shellescape} #{oriented.to_s.shellescape} /dev/null 2>&1`[/\((\d+\.\d+)\)/, 1]
+        nrmse.should_not be_nil
+        nrmse.to_f.should be < 0.005
+      end
+    end
+  end
 end
