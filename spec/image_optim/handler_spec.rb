3e17ad8106984e1986191f8e66a01eee45762ee6
Ivan Kuchin <tadump+git@gmail.com>
2014-07-13 22:11:51 +0200

    limited line length and commented handler_spec

--- spec/image_optim/handler_spec.rb
+++ spec/image_optim/handler_spec.rb
@@ -3,39 +3,50 @@ require 'rspec'
 require 'image_optim/handler'
 
 describe ImageOptim::Handler do
-  it 'should use original as source for first conversion and two temp files for further conversions' do
+  it 'should use original as source for first conversion '\
+      'and two temp files for further conversions' do
     original = double(:original)
-    allow(original).to receive(:temp_path){ fail 'temp_path called unexpectedly' }
+    allow(original).to receive(:temp_path) do
+      fail 'temp_path called unexpectedly'
+    end
 
     handler = ImageOptim::Handler.new(original)
+    temp_a = double(:temp_a)
+    temp_b = double(:temp_b)
+    expect(original).to receive(:temp_path).once.and_return(temp_a)
+    expect(original).to receive(:temp_path).once.and_return(temp_b)
 
-    expect(original).to receive(:temp_path).once.and_return(temp_a = double(:temp_a))
+    # first unsuccessful run
     handler.process do |src, dst|
       expect([src, dst]).to eq([original, temp_a]); false
     end
     expect(handler.result).to be_nil
 
+    # first successful run
     handler.process do |src, dst|
       expect([src, dst]).to eq([original, temp_a]); true
     end
     expect(handler.result).to eq(temp_a)
 
-    expect(original).to receive(:temp_path).once.and_return(temp_b = double(:temp_b))
+    # second unsuccessful run
     handler.process do |src, dst|
       expect([src, dst]).to eq([temp_a, temp_b]); false
     end
     expect(handler.result).to eq(temp_a)
 
+    # second successful run
     handler.process do |src, dst|
       expect([src, dst]).to eq([temp_a, temp_b]); true
     end
     expect(handler.result).to eq(temp_b)
 
+    # third successful run
     handler.process do |src, dst|
       expect([src, dst]).to eq([temp_b, temp_a]); true
     end
     expect(handler.result).to eq(temp_a)
 
+    # forth successful run
     handler.process do |src, dst|
       expect([src, dst]).to eq([temp_a, temp_b]); true
     end
