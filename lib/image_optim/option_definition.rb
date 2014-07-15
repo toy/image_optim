77e8f83fc23929888a7f5ee5a5dc11a1e9fa8e8d
Ivan Kuchin <tadump+git@gmail.com>
2014-07-15 20:31:46 +0200

    moved OptionDefinition into proper namespace

--- lib/image_optim/option_definition.rb
+++ lib/image_optim/option_definition.rb
@@ -1,13 +1,15 @@
-class OptionDefinition
-  attr_reader :name, :default, :type, :description, :proc
+class ImageOptim
+  class OptionDefinition
+    attr_reader :name, :default, :type, :description, :proc
 
-  def initialize(name, default, type, description, &proc)
-    if type.is_a?(String)
-      type, description = default.class, type
+    def initialize(name, default, type, description, &proc)
+      if type.is_a?(String)
+        type, description = default.class, type
+      end
+
+      @name = name.to_sym
+      @description = description.to_s
+      @default, @type, @proc = default, type, proc
     end
-
-    @name = name.to_sym
-    @description = description.to_s
-    @default, @type, @proc = default, type, proc
   end
 end
