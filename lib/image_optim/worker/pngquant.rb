466096416ad246c112b440feedd947bf45c6e4c6
Ivan Kuchin <tadump+git@gmail.com>
2014-08-07 02:09:17 +0200

    pngquant worker (kudos to adammathys for #32 and smasry for #40)

---
+++ lib/image_optim/worker/pngquant.rb
@@ -0,0 +1,43 @@
+require 'image_optim/worker'
+require 'image_optim/option_helpers'
+require 'image_optim/non_negative_integer_range'
+
+class ImageOptim
+  class Worker
+    # http://pngquant.org/
+    class Pngquant < Worker
+      QUALITY_OPTION =
+      option(:quality, 100..100, NonNegativeIntegerRange, 'min..max - don\'t '\
+          'save below min, use less colors below max (both in range `0..100`; '\
+          'in yaml - `!ruby/range 0..100`)') do |v|
+        min = OptionHelpers.limit_with_range(v.begin, 0..100)
+        min..OptionHelpers.limit_with_range(v.end, min..100)
+      end
+
+      SPEED_OPTION =
+      option(:speed, 3, 'speed/quality trade-off: '\
+          '`1` - slow, '\
+          '`3` - default, '\
+          '`11` - fast & rough') do |v|
+        OptionHelpers.limit_with_range(v.to_i, 1..11)
+      end
+
+      # Always run first
+      def run_order
+        -5
+      end
+
+      def optimize(src, dst)
+        args = %W[
+          --quality=#{quality.begin}-#{quality.end}
+          --speed=#{speed}
+          --output=#{dst}
+          --force
+          --
+          #{src}
+        ]
+        execute(:pngquant, *args) && optimized?(src, dst)
+      end
+    end
+  end
+end
