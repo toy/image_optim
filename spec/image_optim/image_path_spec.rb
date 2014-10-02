180ed3283262f53dc651cd8b2e7a17bf13ff10e4
Ivan Kuchin <tadump+git@gmail.com>
2014-10-02 23:16:18 +0200

    use aliased constants in image_path_spec

--- spec/image_optim/image_path_spec.rb
+++ spec/image_optim/image_path_spec.rb
@@ -9,8 +9,8 @@ describe ImageOptim::ImagePath do
     it 'should return ImagePath for string' do
       path = 'a'
 
-      expect(ImagePath.convert(path)).to be_a(ImageOptim::ImagePath)
-      expect(ImagePath.convert(path)).to eq(ImageOptim::ImagePath.new(path))
+      expect(ImagePath.convert(path)).to be_a(ImagePath)
+      expect(ImagePath.convert(path)).to eq(ImagePath.new(path))
 
       expect(ImagePath.convert(path)).not_to eq(path)
       expect(ImagePath.convert(path)).not_to be(path)
@@ -19,18 +19,18 @@ describe ImageOptim::ImagePath do
     it 'should return ImagePath for Pathname' do
       pathname = Pathname.new('a')
 
-      expect(ImagePath.convert(pathname)).to be_a(ImageOptim::ImagePath)
-      expect(ImagePath.convert(pathname)).to eq(ImageOptim::ImagePath.new(pathname))
+      expect(ImagePath.convert(pathname)).to be_a(ImagePath)
+      expect(ImagePath.convert(pathname)).to eq(ImagePath.new(pathname))
 
       expect(ImagePath.convert(pathname)).to eq(pathname)
       expect(ImagePath.convert(pathname)).not_to be(pathname)
     end
 
     it 'should return same instance for ImagePath' do
-      image_path = ImageOptim::ImagePath.new('a')
+      image_path = ImagePath.new('a')
 
-      expect(ImagePath.convert(image_path)).to be_a(ImageOptim::ImagePath)
-      expect(ImagePath.convert(image_path)).to eq(ImageOptim::ImagePath.new(image_path))
+      expect(ImagePath.convert(image_path)).to be_a(ImagePath)
+      expect(ImagePath.convert(image_path)).to eq(ImagePath.new(image_path))
 
       expect(ImagePath.convert(image_path)).to eq(image_path)
       expect(ImagePath.convert(image_path)).to be(image_path)
