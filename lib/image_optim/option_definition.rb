d3ba1d9964510623504c5c4efab3d83ed4062280
Ivan Kuchin <tadump+git@gmail.com>
2014-10-26 23:33:42 +0100

    rename OptionDefinition initialize argument type => type_or_description and check if it is an instance of Class to use for type instead of if is an instance of String to use for description

--- lib/image_optim/option_definition.rb
+++ lib/image_optim/option_definition.rb
@@ -3,9 +3,11 @@ class ImageOptim
   class OptionDefinition
     attr_reader :name, :default, :type, :description, :proc
 
-    def initialize(name, default, type, description, &proc)
-      if type.is_a?(String)
-        type, description = default.class, type
+    def initialize(name, default, type_or_description, description = nil, &proc)
+      if type_or_description.is_a?(Class)
+        type = type_or_description
+      else
+        type, description = default.class, type_or_description
       end
 
       @name = name.to_sym
