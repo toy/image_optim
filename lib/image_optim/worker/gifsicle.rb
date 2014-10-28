a8e447a88549c063cda1df6a8a854cce7d464f0f
Kasper Grubbe <kawsper@gmail.com>
2014-10-28 09:35:24 +0000

    Remove spacing and dots

--- lib/image_optim/worker/gifsicle.rb
+++ lib/image_optim/worker/gifsicle.rb
@@ -8,15 +8,15 @@ class ImageOptim
       option(:interlace, false, 'Turn interlacing on'){ |v| !!v }
 
       LEVEL_OPTION =
-      option(:level, 3, 'Compression level: '\
-          '`1` - stores only the changed portion of each image, '\
-          '`2` - uses transparency to shrink the file further., '\
+      option(:level, 3, 'Compression level:'\
+          '`1` - stores only the changed portion of each image'\
+          '`2` - uses transparency to shrink the file further,'\
           '`3` - several optimization methods (slower, better results)') do |v|
         OptionHelpers.limit_with_range(v.to_i, 1..3)
       end
 
       CAREFUL_OPTION =
-      option(:careful, false, 'Avoid bugs with some software.'){ |v| !!v }
+      option(:careful, false, 'Avoid bugs with some software'){ |v| !!v }
 
       def optimize(src, dst)
         args = %W[
