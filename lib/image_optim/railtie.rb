toy/image_optim

commit 95fe0b93e808c29fb3694fb74a1eaf0871a8cf78

extract options method in railtie

lib/image_optim/railtie.rb
@@ -14,13 +14,15 @@ class ImageOptim
       app.assets
     end
 
-    def register_preprocessor(app)
-      options = if app.config.assets.image_optim == true
+    def options
+      if app.config.assets.image_optim == true
         {}
       else
         app.config.assets.image_optim || {}
       end
+    end
 
+    def register_preprocessor(app)
       image_optim = ImageOptim.new(options)
 
       processor = proc do |_context, data|

commit b14140db2abf5245d5a5f053072abfc25b8d9843

Make app available to options
Seems to have been broken by 95fe0b9

lib/image_optim/railtie.rb
@@ -14,7 +14,7 @@ class ImageOptim
       app.assets
     end
 
-    def options
+    def options(app)
       if app.config.assets.image_optim == true
         {}
       else
@@ -23,7 +23,7 @@ class ImageOptim
     end
 
     def register_preprocessor(app)
-      image_optim = ImageOptim.new(options)
+      image_optim = ImageOptim.new(options(app))
 
       processor = proc do |_context, data|
         image_optim.optimize_image_data(data) || data