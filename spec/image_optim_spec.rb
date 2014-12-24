dba64616419e884ffc67c6eeeb0ca1e0e56cbb15
Ivan Kuchin <tadump+git@gmail.com>
2014-12-25 00:32:15 +0100

    move nrmse and flatten_animation to spec_helper

--- spec/image_optim_spec.rb
+++ spec/image_optim_spec.rb
@@ -52,43 +52,6 @@ describe ImageOptim do
   end
 
   describe :optimize_image do
-    def flatten_animation(image)
-      if image.format == :gif
-        flattened = image.temp_path
-        flatten_command = %W[
-          convert
-          #{image.to_s.shellescape}
-          -coalesce
-          -append
-          #{flattened.to_s.shellescape}
-        ].join(' ')
-        expect(Cmd.run(flatten_command)).to be_truthy
-        flattened
-      else
-        image
-      end
-    end
-
-    def nrmse(image_a, image_b)
-      coalesce_a = flatten_animation(image_a)
-      coalesce_b = flatten_animation(image_b)
-      nrmse_command = %W[
-        compare
-        -metric RMSE
-        -alpha Background
-        #{coalesce_a.to_s.shellescape}
-        #{coalesce_b.to_s.shellescape}
-        /dev/null
-        2>&1
-      ].join(' ')
-      output = Cmd.capture(nrmse_command)
-      if [0, 1].include?($CHILD_STATUS.exitstatus)
-        output[/\((\d+(\.\d+)?)\)/, 1].to_f
-      else
-        fail "compare #{image_a} with #{image_b} failed with `#{output}`"
-      end
-    end
-
     define :have_same_data_as do |expected|
       match{ |actual| actual.binread == expected.binread }
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,40 @@ end
 RSpec.configure do |c|
   c.alias_example_to :they
 end
+
+def flatten_animation(image)
+  if image.format == :gif
+    flattened = image.temp_path
+    flatten_command = %W[
+      convert
+      #{image.to_s.shellescape}
+      -coalesce
+      -append
+      #{flattened.to_s.shellescape}
+    ].join(' ')
+    expect(ImageOptim::Cmd.run(flatten_command)).to be_truthy
+    flattened
+  else
+    image
+  end
+end
+
+def nrmse(image_a, image_b)
+  coalesce_a = flatten_animation(image_a)
+  coalesce_b = flatten_animation(image_b)
+  nrmse_command = %W[
+    compare
+    -metric RMSE
+    -alpha Background
+    #{coalesce_a.to_s.shellescape}
+    #{coalesce_b.to_s.shellescape}
+    /dev/null
+    2>&1
+  ].join(' ')
+  output = ImageOptim::Cmd.capture(nrmse_command)
+  if [0, 1].include?($CHILD_STATUS.exitstatus)
+    output[/\((\d+(\.\d+)?)\)/, 1].to_f
+  else
+    fail "compare #{image_a} with #{image_b} failed with `#{output}`"
+  end
+end
