c3cc2aaf8a9f6d04762c7072d526a3d1bbbebdfb
Ramiro Araujo <rama.araujo@gmail.com>
2015-08-24 22:47:37 -0300

    warn when unknown jpegrecompress compression method is specified, and beter explanation for the methods

--- lib/image_optim/worker/jpegrecompress.rb
+++ lib/image_optim/worker/jpegrecompress.rb
@@ -17,14 +17,20 @@ class ImageOptim
       end.join(', ')
 
       QUALITY_OPTION =
-      option(:quality, 3, "JPEG quality preset: #{quality_names_desc}") do |v|
-        OptionHelpers.limit_with_range(v.to_i, 0...QUALITY_NAMES.length)
+        option(:quality, 3, "JPEG quality preset: #{quality_names_desc}") do |v|
+          OptionHelpers.limit_with_range(v.to_i, 0...QUALITY_NAMES.length)
+        end
+
+      METHOD_OPTION = option(:method, 'ssim', 'Compression method: '\
+          '`mpe` - Mean pixel error, '\
+          '`ssim` - Structural similarity, '\
+          '`ms-ssim` - Multi-scale structural similarity (slow!), '\
+          '`smallfry` - Linear-weighted BBCQ-like (may be patented)') do |v|
+        unless %w[mpe ssim ms-ssim smallfry].include? v
+          warn "Unknown method for jpegrecompress: #{v}"
+        end
+        v
       end
-
-      METHOD_NAMES = [:mpe, :ssim, :'ms-ssim', :smallfry]
-
-      METHOD_OPTION = option(:method, 'ssim',
-                             "Compression method: #{METHOD_NAMES.join(', ')}")
 
       def used_bins
         [:'jpeg-recompress']
