e0605454a730a56a439217a668bd076967aeccd1
Ivan Kuchin <tadump+git@gmail.com>
2016-06-05 15:28:41 +0200

    more specs on pngquant worker quality option

--- spec/image_optim/worker/pngquant_spec.rb
+++ spec/image_optim/worker/pngquant_spec.rb
@@ -10,17 +10,57 @@ describe ImageOptim::Worker::Pngquant do
     end
 
     describe 'value' do
-      let(:options){ {} }
-      subject{ described_class.new(ImageOptim.new, options).quality }
+      let(:subject){ described_class.new(ImageOptim.new, options).quality }
 
-      context 'by default' do
-        it{ is_expected.to eq(100..100) }
+      context 'when lossy not allowed' do
+        context 'by default' do
+          let(:options){ {} }
+
+          it{ is_expected.to eq(100..100) }
+        end
+
+        context 'when value is passed through options' do
+          let(:options){ {:quality => 10..90} }
+
+          it 'warns and keeps default' do
+            expect_any_instance_of(described_class).
+              to receive(:warn).with(/ignored in lossless mode/)
+            is_expected.to eq(100..100)
+          end
+        end
       end
 
-      context 'when lossy allowed by default' do
-        let(:options){ {:allow_lossy => true} }
+      context 'when lossy allowed' do
+        context 'by default' do
+          let(:options){ {:allow_lossy => true} }
 
-        it{ is_expected.to eq(0..100) }
+          it{ is_expected.to eq(0..100) }
+        end
+
+        context 'when value is passed through options' do
+          let(:options){ {:allow_lossy => true, :quality => 10..90} }
+
+          it 'sets the value without warning' do
+            expect_any_instance_of(described_class).not_to receive(:warn)
+            is_expected.to eq(10..90)
+          end
+        end
+
+        context 'when passed range begin is less than 0' do
+          let(:options){ {:allow_lossy => true, :quality => -50..50} }
+
+          it 'sets begin to 0' do
+            is_expected.to eq(0..50)
+          end
+        end
+
+        context 'when passed range end is more than 100' do
+          let(:options){ {:allow_lossy => true, :quality => 50..150} }
+
+          it 'sets end to 100' do
+            is_expected.to eq(50..100)
+          end
+        end
       end
     end
   end
