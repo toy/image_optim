4b45f09b36356e15e4fad87d97896d5b25dfceb9
Adam Doppelt <amd@gurge.com>
2024-11-10 12:21:39 -0800

    svgo --precision

--- lib/image_optim/worker/svgo.rb
+++ lib/image_optim/worker/svgo.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'image_optim/option_helpers'
 require 'image_optim/worker'
 
 class ImageOptim
@@ -16,6 +17,24 @@ class ImageOptim
         Array(v).map(&:to_s)
       end
 
+      ALLOW_LOSSY_OPTION =
+      option(:allow_lossy, false, 'Allow precision option'){ |v| !!v }
+
+      PRECISION_OPTION =
+      option(:precision, 3, 'number of digits in the fractional part ' \
+                            '`0`..`10`, ignored in default/lossless mode') \
+                            do |v, opt_def|
+        if allow_lossy
+          OptionHelpers.limit_with_range(v.to_i, 0..10)
+        else
+          if v != opt_def.default
+            warn "#{self.class.bin_sym} #{opt_def.name} #{v} ignored " \
+                 'in default/lossless mode'
+          end
+          opt_def.default
+        end
+      end
+
       def optimize(src, dst, options = {})
         args = %W[
           --input #{src}
@@ -27,6 +46,7 @@ class ImageOptim
         enable_plugins.each do |plugin_name|
           args.unshift "--enable=#{plugin_name}"
         end
+        args.unshift "--precision=#{precision}" if allow_lossy
         execute(:svgo, args, options) && optimized?(src, dst)
       end
     end
